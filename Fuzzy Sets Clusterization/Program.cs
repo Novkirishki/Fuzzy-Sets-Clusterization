3dbe2a8a08ce4904f8f119e5e42bc19f96111a35
Novkirishki <novkirishki94@gmail.com>
2018-02-06 20:09:21 +0200

    Initial

---
+++ Fuzzy Sets Clusterization/Program.cs
@@ -0,0 +1,17 @@
+﻿using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzzy_Sets_Clusterization
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            excel.ReadOnly = true;
+        }
+    }
+}
---
+++ Fuzzy Sets Clusterization/SkillData.cs
@@ -0,0 +1,44 @@
+﻿using LinqToExcel.Attributes;
+
+namespace Fuzzy_Sets_Clusterization
+{
+    internal class SkillData
+    {
+        [ExcelColumn("middleF1")]
+        public double Middle1 { get; set; }
+
+        [ExcelColumn("middleF2")]
+        public double Middle2 { get; set; }
+
+        [ExcelColumn("middleF3")]
+        public double Middle3 { get; set; }
+
+        [ExcelColumn("middleF4")]
+        public double Middle4 { get; set; }
+
+        [ExcelColumn("hardF1")]
+        public double Hard1 { get; set; }
+
+        [ExcelColumn("hardF2")]
+        public double Hard2 { get; set; }
+
+        [ExcelColumn("hardF3")]
+        public double Hard3 { get; set; }
+
+        [ExcelColumn("hardF4")]
+        public double Hard4 { get; set; }
+
+        [ExcelColumn("softF1")]
+        public double Soft1 { get; set; }
+
+        [ExcelColumn("softF2")]
+        public double Soft2 { get; set; }
+
+        [ExcelColumn("softF3")]
+        public double Soft3 { get; set; }
+
+        [ExcelColumn("softF4")]
+        public double Soft4 { get; set; }
+
+    }
+}
